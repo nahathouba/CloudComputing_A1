f0f687397de2a18beb0148a0ae6c3e0b3775d6b3
Ningthouba Nahakpam <s3733745@student.rmit.edu.au>
2023-04-08 07:19:47 +1000

    Completed till task-4

--- Utils.py
+++ Utils.py
@@ -70,7 +70,79 @@ def putUsers(table):
             print(f"User added for email: {user['email']}")
 
 
+def createUser(newUser):
+    dynamodb = boto3.resource(
+        'dynamodb',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        aws_session_token=aws_session_token,
+        region_name=region_name
+    )
+
+    table = dynamodb.Table(loginTableName)
+    table.put_item(Item=newUser)
+    print(f"User added for email: {newUser['email']}")
+
+
+def userExists(userEmail):
+    dynamodb = boto3.resource(
+        'dynamodb',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        aws_session_token=aws_session_token,
+        region_name=region_name
+    )
+
+    table = dynamodb.Table(loginTableName)
+    response = table.get_item(
+        Key={
+            'email': userEmail
+        }
+    )
+    return 'Item' in response
+
+
+def verifyUser(userEmail, userPassword):
+    dynamodb = boto3.resource(
+        'dynamodb',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        aws_session_token=aws_session_token,
+        region_name=region_name
+    )
+
+    table = dynamodb.Table(loginTableName)
+    response = table.get_item(
+        Key={
+            'email': userEmail
+        }
+    )
+    if 'Item' in response:
+        return response['Item']['password'] == userPassword
+    return False
+
+
+def getUserName(userEmail):
+    dynamodb = boto3.resource(
+        'dynamodb',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        aws_session_token=aws_session_token,
+        region_name=region_name
+    )
+
+    table = dynamodb.Table(loginTableName)
+    response = table.get_item(
+        Key={
+            'email': userEmail
+        }
+    )
+    if 'Item' in response:
+        return response['Item']['username']
+    return None
+
 # Music Table Helpers
+
 
 def loadMusic(table):
     istableExists = tableExists(musicTableName)
