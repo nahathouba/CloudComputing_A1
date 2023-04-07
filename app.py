c8133c9b03e2535175e71f9b977f02c50971dd87
Ningthouba Nahakpam <s3733745@student.rmit.edu.au>
2023-04-08 04:20:17 +1000

    Login and Register basic functionality complated

--- app.py
+++ app.py
@@ -1,38 +1,54 @@
-from flask import Flask, render_template, request
-from Utils import tableExists, createLogInTable, createMusicTable
-from S3Utils import downloadArtistImage, uploadArtistImageS3, createArtistImageBucket
+from flask import Flask, render_template, request, url_for, flash, redirect
+from utils import tableExists, createLogInTable, createMusicTable
+from s3Utils import downloadArtistImage, uploadArtistImageS3, createArtistImageBucket
+from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8693f210b860a5ab14b3269295d1d203'
 
 
-isTableExist = tableExists('login')
-if not isTableExist:
-    # Create the login table and load the users
-    createLogInTable()
+# isTableExist = tableExists('login')
+# if not isTableExist:
+# Create the login table and load the users
+# createLogInTable()
 
-isTableExist = tableExists('music')
-if not isTableExist:
-    # Create the music table and load the music
-    createMusicTable()
+# isTableExist = tableExists('music')
+# if not isTableExist:
+# Create the music table and load the music
+# createMusicTable()
 
 # Create the artist image S3 bucket
-createArtistImageBucket()
+# createArtistImageBucket()
 # Download the artist images from the URL
-downloadArtistImage()
+# downloadArtistImage()
 # Upload the artist images to the S3 bucket
-uploadArtistImageS3()
+# uploadArtistImageS3()
 
 
-@ app.route("/")
+@ app.route('/')
 def index():
     return render_template("index.html")
 
-# TODO: Create a route for /checkLogin
+
+@ app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('login'))
+    return render_template("register.html", form=form, title='Register')
 
 
-@ app.route("/checkLogin", methods=["POST"])
-def checkLogin():
-    email = request.form.get("email")
+@ app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'nahakpamningthouba@gmail.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful! Email or Password is invalid.', 'danger')
+    return render_template("login.html", form=form, title='Login')
 
 
 if __name__ == "__main__":
